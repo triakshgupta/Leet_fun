441e956bdba77818199cddad3121f6c3f0b1c425
triaksh <107975299+triakshgupta@users.noreply.github.com>
2022-09-26 09:15:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.35%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -16,14 +16,30 @@ public:
     // }
     
     
+    // Tabulation
+    // int fib(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     dp[0]=0;
+    //     if(n>=1) dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
     
-    int fib(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0]=0;
-        if(n>=1) dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+    
+    int fib(int n){
+        if(n==0){
+            return 0;
         }
-        return dp[n];
+        int prev=1;
+        int prevp=0;
+        int curr;
+        for(int i=2;i<=n;i++){
+            curr=prev+prevp;
+            prevp=prev;
+            prev=curr;
+        }
+        return prev;
     }
 };
