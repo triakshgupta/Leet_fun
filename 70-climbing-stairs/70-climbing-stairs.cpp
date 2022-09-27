92da185f79c9567ff1d2fe340c18d72d8f8493cc
triaksh <107975299+triakshgupta@users.noreply.github.com>
2022-09-27 18:16:04 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.43%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -23,13 +23,28 @@ public:
     
     
     //  Tabulation
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     dp[0]=1;
+    //     if(n>=1) dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+    
+    
+    // Space optimized
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        if(n>=1) dp[1]=1;
+        int cur;
+        int prev=1;
+        int prevp=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            cur=prev+prevp;
+            prevp=prev;
+            prev=cur;
         }
-        return dp[n];
+        return prev;
     }
+    
 };
