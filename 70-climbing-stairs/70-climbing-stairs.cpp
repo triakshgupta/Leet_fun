8144bb5821c4ed0f9b590a71cdc7d5ad0e3122ca
triaksh <107975299+triakshgupta@users.noreply.github.com>
2022-09-27 14:15:29 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.84%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countmove(int i,vector<int>& dp){
+        if(i<=1){
+            return 1;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int one=countmove(i-1,dp);
+        int two=0;
+        if(i>1){
+            two=countmove(i-2,dp);
+        }
+        return dp[i]=one+two;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return countmove(n,dp);
+    }
+};
