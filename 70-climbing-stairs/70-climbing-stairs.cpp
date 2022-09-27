aad8567f029ca2dd6c179e8879f38519601faa3b
triaksh <107975299+triakshgupta@users.noreply.github.com>
2022-09-27 18:09:23 +0530

    Time: 5 ms (13.42%), Space: 6.2 MB (31.84%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    int countmove(int i,vector<int>& dp){
-        if(i<=1){
-            return 1;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int one=countmove(i-1,dp);
-        int two=0;
-        if(i>1){
-            two=countmove(i-2,dp);
-        }
-        return dp[i]=one+two;
-    }
+    
+    // Memoization
+    // int countmove(int i,vector<int>& dp){
+    //     if(i<=1){
+    //         return 1;
+    //     }
+    //     if(dp[i]!=-1){
+    //         return dp[i];
+    //     }
+    //     int one=countmove(i-1,dp);
+    //     int two=0;
+    //     if(i>1){
+    //         two=countmove(i-2,dp);
+    //     }
+    //     return dp[i]=one+two;
+    // }
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     return countmove(n,dp);
+    // }
+    
+    
+    //  Tabulation
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return countmove(n,dp);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            dp[i]=dp[i-1];
+            if(i>=2) dp[i]+=dp[i-2];
+        }
+        return dp[n];
     }
 };
