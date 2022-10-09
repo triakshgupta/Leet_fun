9f6684208dce434e663dd40552f296a86f84b31e
triaksh <107975299+triakshgupta@users.noreply.github.com>
2022-10-09 09:02:45 +0530

    Time: 81 ms (45.38%), Space: 36.7 MB (89.43%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,6 +11,33 @@
  */
 class Solution {
 public:
+    // void inorder(TreeNode* x,vector<int>& a){
+    //     if(x==NULL){
+    //         return;
+    //     }
+    //     inorder(x->left,a);
+    //     a.push_back(x->val);
+    //     inorder(x->right,a);
+    // }
+    // bool findTarget(TreeNode* root, int k) {
+    //     vector<int> a;
+    //     inorder(root,a);
+    //     int i=0,j=a.size()-1;
+    //     while(i<j){
+    //         if(a[i]+a[j]==k){
+    //             return true;
+    //         }
+    //         else if(k>a[i]+a[j]){
+    //             i++;
+    //         }
+    //         else{
+    //             j--;
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    
     void inorder(TreeNode* x,vector<int>& a){
         if(x==NULL){
             return;
@@ -19,19 +46,24 @@ public:
         a.push_back(x->val);
         inorder(x->right,a);
     }
+    bool search(TreeNode* x,int a){
+        if(x==NULL){
+            return false;
+        }
+        if(x->val==a){
+            return true;
+        }
+        else if(x->val>a){
+            return search(x->left,a);
+        }
+        return search(x->right,a);
+    }
     bool findTarget(TreeNode* root, int k) {
         vector<int> a;
         inorder(root,a);
-        int i=0,j=a.size()-1;
-        while(i<j){
-            if(a[i]+a[j]==k){
+        for(int i=0;i<a.size();i++){
+            if(k-a[i]!=a[i] && search(root,k-a[i])){
                 return true;
-            }
-            else if(k>a[i]+a[j]){
-                i++;
-            }
-            else{
-                j--;
             }
         }
         return false;
