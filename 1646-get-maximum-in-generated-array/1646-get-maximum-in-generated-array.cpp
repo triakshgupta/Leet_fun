b62b3814a9b19ca929911d743d220207402ddbe7
triaksh <107975299+triakshgupta@users.noreply.github.com>
2022-10-28 19:33:31 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (27.03%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int temp(int n){
+    int temp(int n,vector<int>& dp){
         if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
         if(n%2!=0){
-            return temp((n-1)/2) +temp((n+1)/2);
+            return dp[n]=temp((n-1)/2,dp) +temp((n+1)/2,dp);
         }
         else{
-            return temp(n/2);
+            return dp[n]=temp(n/2,dp);
         }
     }
     int getMaximumGenerated(int n) {
         int ans=0;
+        vector<int> dp(n+1,-1);
         for(int i=1;i<=n;i++){
-            ans=max(ans,temp(i));
+            ans=max(ans,temp(i,dp));
         }
         return ans;
     }
